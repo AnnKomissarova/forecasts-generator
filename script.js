23111bdb279f2ba5f8ca8765c466162eb9b2f6bf
AnnKomissarova <nnkmssrv@gmail.com>
2022-10-27 15:59:41 +0300

    Hometask - 5

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-/* Генерация предсказания должна происходить при клике на кнопку «предсказать судьбу» */
+const container = document.querySelector('.container');
+const generate = document.querySelector('.forecast-btn');
+const forecast = document.querySelector('.current-forecast h1');
+const forecastPerCent = document.querySelector('.current-forecast p');
+const list = document.querySelector('#forecast-item');
 
-/* Заранее заготовь 3-5 предсказаний и в зависимости от того, как лягут карты судьбы (или что скажет Math.random) показывай их пользователю */
+const forecastes = ['У тебя все получится!', 'Ты найдешь то,что ищешь', 'Тебя ждет повышение!', 'Тебя ждет встреча с друзьми!', 'Просто расслабься и выпей вина =)']
 
-/* Подставляй текст нового предсказания в .current-forecast h1 */
+generate.addEventListener('click', () => {
+    let randForecast = Math.floor(Math.random() * forecastes.length);
+    forecast.innerHTML = forecastes[randForecast];
+    forecastPerCent.innerHTML = 'Вероятность: ' + Math.floor(Math.random() * 101) + '%';
 
-/* Показывай процент вероятности, с которым предсказание сбудется — в верстке это .current-forecast p */
+    const map = makeMapForecast(forecast,forecastPerCent);
+    container.append(map);
+});
 
-/* Данный процент также нужно генерировать автоматически, он может принимать значения от 0 до 100% */
+function makeMapForecast(forecast,forecastPerCent) {
+    const forecastMap = list.content.cloneNode(true);
 
-/* Совет: заведи функцию-хелпер, которая будет заниматься только генерацией данных в диапазоне от min до max и используй ее где нужно */
+    forecastMap.querySelector('h3').textContent = forecast.textContent;
+    forecastMap.querySelector('p').textContent = forecastPerCent.textContent;
 
-/* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
+    return forecastMap;
+}
 
-/* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
